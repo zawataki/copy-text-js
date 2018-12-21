7c37113329671d5c00abd3320d39c4f449b68c2d
Yuki Takizawa <y.takizawa64@gmail.com>
2018-12-21 10:00:14 +0900

    Use doc instead of document

--- copy-text-to-clipboard.js
+++ copy-text-to-clipboard.js
@@ -2,15 +2,15 @@
  * @param  {String} textVal A text to copy to clipboard
  * @returns {boolean} true if succeeded. Otherwise false
  */
-function copyTextToClipboard(textVal) {
-    let copyFrom = document.createElement("textarea");
+function copyTextToClipboard(doc, textVal) {
+    let copyFrom = doc.createElement("textarea");
     copyFrom.textContent = textVal;
 
-    let bodyElm = document.getElementsByTagName("body")[0];
+    let bodyElm = doc.getElementsByTagName("body")[0];
     bodyElm.appendChild(copyFrom);
 
     copyFrom.select();
-    let retVal = document.execCommand('copy');
+    let retVal = doc.execCommand('copy');
     bodyElm.removeChild(copyFrom);
 
     return retVal;
